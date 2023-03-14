78c6c611268d81194de52b939317336ffb2a8424
AymenMzoughi <aymen.mzoughi@esprit.tn>
2023-03-14 22:23:33 +0100

    Update User

--- User/userService.js
+++ User/userService.js
@@ -51,6 +51,23 @@ const deleteUser = async (req, res, next) => {
     }
   };
 
+const updateUser = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const updatedUser = await UserModel.findByIdAndUpdate(id, req.body, {
+        useFindAndModify: false,
+        new: true, // pour renvoyer le document mis à jour plutôt que l'ancien document
+      });
+      if (!updatedUser) {
+        return res.status(404).json({ message: ERROR_MESSAGES.USER_NOT_FOUND });
+      }
+      res.json(updatedUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+    }
+  };
+
 
   
   module.exports = { addUser, showUsers, deleteUser, updateUser, findUser };
