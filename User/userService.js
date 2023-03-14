debea2f2c0a8d72237ee0e506b2f982aaab3788c
AymenMzoughi <aymen.mzoughi@esprit.tn>
2023-03-14 22:22:26 +0100

    Delete User

--- User/userService.js
+++ User/userService.js
@@ -36,6 +36,21 @@ const showUsers = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res, next) => {
+    try {
+      const userToDelete = await UserModel.findById(req.params.id, 'nom');
+      if (!userToDelete) {
+        return res.status(404).json({ message: ERROR_MESSAGES.USER_NOT_FOUND });
+      }
+      await UserModel.findByIdAndDelete(req.params.id);
+      const docs = await UserModel.find({});
+      res.status(200).json({ message: `User ${userToDelete.nom} is deleted!`, docs });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+    }
+  };
+
 
   
   module.exports = { addUser, showUsers, deleteUser, updateUser, findUser };
