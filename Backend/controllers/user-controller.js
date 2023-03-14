eb478263522e32a34ed1d6cd3792e27b02144fdb
zaineb-bouallegui <92609445+zaineb-bouallegui@users.noreply.github.com>
2023-03-15 00:06:04 +0100

    Login

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -12,42 +12,109 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const express = require('express');
 const router = express.Router();
 
-const signup = async (req, res, next) => {
-    const {firstname,lastname,Number,pdp,email,role,password} = req.body;
+
+
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+
     let existingUser;
-try{
-    existingUser = await User.findOne({ email: email});
-} catch (err) {
-    console.log(err);
-}
-if (existingUser) {
-    return res
-    .status(400)
-    .json({message: "User already exists! Login Instead" });
-}
-const hashedPassword = bcrypt.hashSync (password);
-const user = new User ({
-        firstname,
-        lastname,
-        Number,
-        pdp,
-        email,
-        password: hashedPassword,
-        role,
-    });
-
-
     try {
-        await user.save();
-     }  catch (err) {
-        console.log(err);
+        existingUser = await User.findOne({ email: email });
+    } catch (err) {
+        return new Error(err);
     }
+    if (!existingUser) {
+        return res.status(401).json({message:"User not found. Signup Please"})
+    }
+    const isPasswordCorrect = bcrypt.compareSync(password,existingUser.password);
+    if (!isPasswordCorrect) {
+        return res.status(401).json({message:'Invalid Password'})
+    }
+const token = jwt.sign({id: existingUser._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "35s",
 
-    return res.status(201).json({ message:user });
+});
+console.log("Generated Token\n", token);
+res.cookie(String(existingUser._id), token, {
+    path: "/",
+    expires: new Date(Date.now() + 1000 * 30),
+    httpOnly: true,
+    sameSite: 'lax',
+});
+existingUser.token = token;
+    return res
+    .status(200)
+    .json({message:'Successfully Logged In', user: existingUser });
 };
 
+const verifyToken = (req, res, next) => {
+    const cookies = req.headers.cookie;
+  
+//    const headers = req.headers [`authorization`] ;
+    const token = cookies.split("= ")[1];
+    console.log(token);
+ if (!token) {
+    
+    res.status(404).json({message:"No token found"})
+   }
+         jwt.verify(String(token), process.env.JWT_SECRET_KEY, (err, user) => {
+            if (err) {
+             return res.status(400).json({message:"Invalid Token"})
+          }
+           console.log(user.id);
+             req.id = user.id;
+        });
+       next();
+    } ;
+const getUser = async (req, res, next) => {
+const userId = req.id;
+let user;
+try {
+    user = await User.findById(userId, "-password");
+} catch (err) {
+    return new Error(err)
+}
+if (!user) {
+    return res.status(404).json({message: "User Not Found"})
+}
+return res.status(200).json({user})
+}
+const refreshToken = (req,res,next) => {
+    const cookies = req.headers.cookie;
+    const prevToken = cookies.split("=")[1];
+    if(!prevToken) {
+        return res.status(400).json({message: "Couldn't find token"});
+    }
+    jwt.verify(String(prevToken),process.env.JWT_SECRET_KEY,(err,user) => {
+        if (err) {
+            console.log(err);
+            return res.status(403).json({message: "Authentification failed "});
+        }
+        res.clearCookie(`${user.id }`);
+        req.cookies[`${user.id}`] = "";
 
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+            expiresIn:"35s",
+        });
 
+        console.log("Regenerated Token\n", token);
+
+// if(req.cookies[`${existingUser._id}`]) {
+//     req.cookies[`${existingUser._id}`] = ""
+// }
+
+res.cookie(String(user.id), token, {
+    path: "/",
+    expires: new Date(Date.now() + 1000 * 30), //30s
+    httpOnly: true,
+    sameSite: "lax",
+});
+
+req.id = user.id;
+next();
+    });
+};
+ 
 
 
 
