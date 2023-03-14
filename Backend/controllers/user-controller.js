b12589e47f01e553c77eb2af68008bb8a675b96d
zaineb-bouallegui <92609445+zaineb-bouallegui@users.noreply.github.com>
2023-03-15 00:16:14 +0100

    RefreshToken

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -14,28 +14,43 @@ const router = express.Router();
 
 
 
-const verifyToken = (req, res, next) => {
+
+const refreshToken = (req,res,next) => {
     const cookies = req.headers.cookie;
-  
-//    const headers = req.headers [`authorization`] ;
-    const token = cookies.split("= ")[1];
-    console.log(token);
- if (!token) {
-    
-    res.status(404).json({message:"No token found"})
-   }
-         jwt.verify(String(token), process.env.JWT_SECRET_KEY, (err, user) => {
-            if (err) {
-             return res.status(400).json({message:"Invalid Token"})
-          }
-           console.log(user.id);
-             req.id = user.id;
+    const prevToken = cookies.split("=")[1];
+    if(!prevToken) {
+        return res.status(400).json({message: "Couldn't find token"});
+    }
+    jwt.verify(String(prevToken),process.env.JWT_SECRET_KEY,(err,user) => {
+        if (err) {
+            console.log(err);
+            return res.status(403).json({message: "Authentification failed "});
+        }
+        res.clearCookie(`${user.id }`);
+        req.cookies[`${user.id}`] = "";
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+            expiresIn:"35s",
         });
-       next();
-    } ;
 
+        console.log("Regenerated Token\n", token);
 
+// if(req.cookies[`${existingUser._id}`]) {
+//     req.cookies[`${existingUser._id}`] = ""
+// }
 
+res.cookie(String(user.id), token, {
+    path: "/",
+    expires: new Date(Date.now() + 1000 * 30), //30s
+    httpOnly: true,
+    sameSite: "lax",
+});
+
+req.id = user.id;
+next();
+    });
+};
+ 
 
 
 
