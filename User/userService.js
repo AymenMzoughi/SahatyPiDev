99d1a0718cefdc312cf21578e2a41f0e7c0870ec
AymenMzoughi <aymen.mzoughi@esprit.tn>
2023-03-14 22:24:06 +0100

    Find user

--- User/userService.js
+++ User/userService.js
@@ -68,7 +68,21 @@ const updateUser = async (req, res) => {
     }
   };
 
-
+  const findUser = async (req, res) => {
+    try {
+      const { searchValue } = req.query;
+      const users = await UserModel.find({ $or: [
+        { nom: new RegExp(searchValue, 'i') },
+        { prenom: new RegExp(searchValue, 'i') },
+        { mail: new RegExp(searchValue, 'i') },
+        { numero: new RegExp(searchValue, 'i') },
+      ]});
+      res.json(users);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+    }
+  };
   
   module.exports = { addUser, showUsers, deleteUser, updateUser, findUser };
     
